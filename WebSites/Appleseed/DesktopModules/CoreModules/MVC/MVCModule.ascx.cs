7e5efe9194b119b61c00a5bd41aee4f86dce983f
Germ?n Schnyder <german.schnyder@agileworks.com.uy>
2011-06-15 10:03:29 -0300

    Some minor script refactoring

--- WebSites/Appleseed/DesktopModules/CoreModules/MVC/MVCModule.ascx.cs
+++ WebSites/Appleseed/DesktopModules/CoreModules/MVC/MVCModule.ascx.cs
@@ -11,48 +11,16 @@ using System.Web.UI.HtmlControls;
 using System.Xml.Linq;
 using Appleseed.Framework;
 
-public partial class DesktopModules_CoreModules_MVC_MVCModule :  MVCModuleControl
+public partial class DesktopModules_CoreModules_MVC_MVCModule : MVCModuleControl
 {
-
-    protected override void OnLoad(EventArgs e)
-    {
-        base.OnLoad(e);
-
-        if (!Page.ClientScript.IsClientScriptBlockRegistered("extra"))
-        {
-            string extraScripts = GetExtraScripts();
-            Page.ClientScript.RegisterClientScriptBlock(GetType(), "extra", extraScripts, false);
-        }
-    }
-
-
-    private string GetExtraScripts()
-    {
-        string scripts = string.Empty;
-        try
-        {
-            string filePath = HttpContext.Current.Server.MapPath("~/Scripts/Scripts.xml");
-            XDocument xml = XDocument.Load(filePath);
-            foreach (var s in xml.Descendants("scripts").DescendantNodes())
-            {
-                scripts += s.ToString() + Environment.NewLine;
-            }
-        }
-        catch (Exception exc)
-        {
-            ErrorHandler.Publish(LogLevel.Debug, exc);
-        }
-        return scripts;
-    }
-
-
+    
     protected void Page_Load(object sender, EventArgs e)
     {
     }
 
     public DesktopModules_CoreModules_MVC_MVCModule()
     {
-       
+
     }
 
 
