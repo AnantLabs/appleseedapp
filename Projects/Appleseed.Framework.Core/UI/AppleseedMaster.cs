cec4b1bbd6a897d5f44c1bcb76fdc39d2a5c9935
Germ?n Schnyder <german.schnyder@agileworks.com.uy>
2011-06-16 14:57:09 -0300

    Added generalerror.aspx

---
+++ Projects/Appleseed.Framework.Core/UI/AppleseedMaster.cs
@@ -0,0 +1,109 @@
+﻿using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Collections.Generic;
+using System.Collections;
+using Appleseed.Framework.Site.Configuration;
+using System.Web.Mvc;
+using System.Web;
+using System.Xml.Linq;
+using System;
+using Appleseed.Framework;
+
+namespace Appleseed
+{
+    public class AppleseedMaster : System.Web.Mvc.ViewMasterPage
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnLoad(System.EventArgs e)
+        {
+            InsertAllScripts(Page, Context);
+
+            base.OnLoad(e);
+        }
+
+        public static void InsertAllScripts(Page page, HttpContext context)
+        {
+            if (!page.ClientScript.IsClientScriptBlockRegistered("allscripts")) {
+                var scripts = GetBaseScripts();
+
+                int index = 0;
+                foreach (var script in scripts) {
+                    HtmlGenericControl include = new HtmlGenericControl("script");
+                    include.Attributes.Add("type", "text/javascript");
+                    include.Attributes.Add("src", script as string);
+                    page.Header.Controls.AddAt(index++, include);
+                }
+                
+                var portalSettings = (PortalSettings)context.Items["PortalSettings"];
+
+                if (portalSettings != null) {
+                    var cssHref = "/Design/jqueryUI/" + portalSettings.PortalAlias + "/jquery-ui.custom.css";
+
+                    HtmlGenericControl include = new HtmlGenericControl("link");
+                    include.Attributes.Add("type", "text/css");
+                    include.Attributes.Add("rel", "stylesheet");
+                    include.Attributes.Add("href", cssHref);
+                    page.Header.Controls.AddAt(index++, include);
+                }
+
+                var uiculture = System.Threading.Thread.CurrentThread.CurrentCulture.TwoLetterISOLanguageName;
+                var datepickerscript = "$(document).ready(function(){$.datepicker.setDefaults($.datepicker.regional['" + uiculture + "']);});";
+
+                HtmlGenericControl includedp = new HtmlGenericControl("script");
+                includedp.Attributes.Add("type", "text/javascript");
+                includedp.InnerHtml = datepickerscript;
+                page.Header.Controls.AddAt(index++, includedp);
+
+
+                string extraScripts = GetExtraScripts();
+                page.ClientScript.RegisterClientScriptBlock(page.GetType(), "allscripts", extraScripts, false);
+            }
+        }
+
+        public static string GetExtraScripts()
+        {
+            string scripts = string.Empty;
+            try {
+                string filePath = HttpContext.Current.Server.MapPath("~/Scripts/Scripts.xml");
+                XDocument xml = XDocument.Load(filePath);
+                foreach (var s in xml.Descendants("scripts").DescendantNodes()) {
+                    scripts += s.ToString() + Environment.NewLine;
+                }
+            } catch (Exception exc) {
+                ErrorHandler.Publish(LogLevel.Debug, exc);
+            }
+
+            return scripts;
+        }
+
+        public static ArrayList GetBaseScripts()
+        {
+            var scripts = new ArrayList();
+
+            scripts.Add("/aspnet_client/jQuery/jquery-1.6.1.min.js");
+            scripts.Add("/aspnet_client/jQuery/jquery-ui-1.8.11.min.js");
+            scripts.Add("/aspnet_client/jQuery/jquery.validate.min.js");
+            scripts.Add("/aspnet_client/jQuery/jquery.validate.unobtrusive.min.js");
+            scripts.Add("/aspnet_client/jQuery/jquery.bgiframe.min.js");
+            scripts.Add("/aspnet_client/jQuery/jquery-ui-i18n.min.js");
+            scripts.Add("/aspnet_client/jQuery/jquery.unobtrusive-ajax.min.js");
+
+            scripts.Add("/aspnet_client/jQuery/modernizr-1.7.min.js");
+            scripts.Add("/aspnet_client/jQuery/jquery.cookie.js");
+
+            scripts.Add("/aspnet_client/jQuery/MicrosoftAjax.js");
+            scripts.Add("/aspnet_client/jQuery/MicrosoftMvcAjax.js");
+            scripts.Add("/aspnet_client/jQuery/MicrosoftMvcValidation.js");
+
+            scripts.Add("/aspnet_client/js/DragNDrop.js");
+            scripts.Add("/aspnet_client/js/browser_upgrade_notification.js");
+
+            scripts.Add("/aspnet_client/CSSControlAdapters/AdapterUtils.js");
+            scripts.Add("/aspnet_client/CSSControlAdapters/MenuAdapter.js");
+            return scripts;
+        }
+    }
+}
---
+++ WebSites/Appleseed/DesktopModules/CoreModules/SignIn/SignInSocialNetwork.ascx.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Facebook.Web;
+using System.Web.Security;
+using Appleseed.Framework.Site.Configuration;
+using Facebook;
+using Appleseed.Framework.Security;
+using Appleseed.Framework;
+using System.Web.Profile;
+using blowery.Web.HttpCompress;
+using Appleseed.Framework.UI.WebControls;
+using Appleseed.Framework.Providers.AppleseedMembershipProvider;
+using System.Net.Mail;
+using Appleseed.Framework.Helpers;
+using System.Text;
+using Appleseed.Framework.Settings;
+using Twitterizer;
+using System.Security.Cryptography;
+
+namespace Appleseed.DesktopModules.CoreModules.SignIn
+{
+    public partial class SignInSocialNetwork : SignInControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var facebookContext = GetFacebookWebContext();
+            if (facebookContext != null) {
+                appId.Value = PortalSettings.CustomSettings["SITESETTINGS_FACEBOOK_APP_ID"].ToString();
+                if (facebookContext.IsAuthenticated()) {
+                    //Here is were i check if the user login via facebook
+                    FacebookSignInMethod();
+                }
+            } else {
+                //TODO: ocultar boton y mostrar warning
+                loginfb_div.Visible = false;
+                errfb.Visible = true;
+            }
+
+            var TwitterRequestToken = GetTwitterRequestToken();
+            if (TwitterRequestToken != null) {
+                Uri authenticationUri = OAuthUtility.BuildAuthorizationUri(TwitterRequestToken.Token, true);
+
+                string url = authenticationUri.AbsoluteUri;
+                LogIn.Text = "Log in Twitter";
+                LogIn.NavigateUrl = url;
+
+
+
+            } else {
+                //TODO: ocultar boton y mostrar warning
+                logintwit_div.Visible = false;
+                errtwit.Visible = true;
+            }
+        }
+
+        private void UpdateProfile()
+        {
+            var client = new FacebookWebClient();
+            dynamic me = client.Get("me");
+
+            ProfileManager.Provider.ApplicationName = PortalSettings.PortalAlias;
+            ProfileBase profile = ProfileBase.Create(me.email);
+            profile.SetPropertyValue("Email", me.email);
+            profile.SetPropertyValue("Name", me.name);
+            try {
+                profile.Save();
+            } catch (Exception exc) {
+                ErrorHandler.Publish(LogLevel.Error, "Error al salvar un perfil", exc);
+            }
+        }
+
+        #region Properties
+
+        public override Guid GuidID
+        {
+            get
+            {
+                return new Guid("{008D65E2-FB7E-4B00-BC2E-7491388BACDE}");
+            }
+        }
+
+        #endregion
+
+        #region Facebook Method
+
+        /// <summary>
+        /// check if facebook settings were setting up from the portal settings, if not update the facebooksettings section of the web config file
+        /// </summary>
+        internal FacebookWebContext GetFacebookWebContext()
+        {
+            if (PortalSettings.CustomSettings.ContainsKey("SITESETTINGS_FACEBOOK_APP_ID") &&
+                !PortalSettings.CustomSettings["SITESETTINGS_FACEBOOK_APP_ID"].ToString().Equals(string.Empty) &&
+                PortalSettings.CustomSettings.ContainsKey("SITESETTINGS_FACEBOOK_APP_SECRET") &&
+                !PortalSettings.CustomSettings["SITESETTINGS_FACEBOOK_APP_SECRET"].ToString().Equals(string.Empty)) {
+                string appId = PortalSettings.CustomSettings["SITESETTINGS_FACEBOOK_APP_ID"].ToString();
+                var appSecret = PortalSettings.CustomSettings["SITESETTINGS_FACEBOOK_APP_SECRET"].ToString();
+
+                if (FacebookWebContext.Current.Settings != null) {
+                    var facebookConfigurationSection = new FacebookConfigurationSection();
+                    facebookConfigurationSection.AppId = appId;
+                    facebookConfigurationSection.AppSecret = appSecret;
+                    return new FacebookWebContext(facebookConfigurationSection);
+                }
+            }
+
+            return null;
+        }
+
+        private void FacebookSignInMethod()
+        {
+            var client = new FacebookWebClient();
+            dynamic me = client.Get("me");
+            if (Membership.GetUser(me.email) == null) {
+                Session["CameFromSocialNetwork"] = true;
+                Session["FacebookUserName"] = me.email;
+                Session["FacebookName"] = me.name;
+
+                Session["FacebookPassword"] = GeneratePasswordHash(me.email);
+                string urlRegister = ConvertRelativeUrlToAbsoluteUrl("~/DesktopModules/CoreModules/Register/Register.aspx");
+                Response.Redirect(urlRegister);
+            } else
+                PortalSecurity.SignOn(me.email, GeneratePasswordHash(me.email));
+            
+        }
+
+        public string GeneratePasswordHash(string thisPassword)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            byte[] tmpSource;
+            byte[] tmpHash;
+
+            tmpSource = ASCIIEncoding.ASCII.GetBytes(thisPassword); // Turn password into byte array
+            tmpHash = md5.ComputeHash(tmpSource);
+
+            StringBuilder sOutput = new StringBuilder(tmpHash.Length);
+            for (int i = 0; i < tmpHash.Length; i++) {
+                sOutput.Append(tmpHash[i].ToString("X2"));  // X2 formats to hexadecimal
+            }
+            return sOutput.ToString();
+        }
+
+        public override void Logoff()
+        {
+            var context = GetFacebookWebContext();
+            if (context != null && context.IsAuthenticated()) {
+                context.DeleteAuthCookie();
+            }
+            Session.RemoveAll();
+        }
+
+        #endregion Facebook Methods
+
+        #region Twitter Method
+
+        internal OAuthTokenResponse GetTwitterRequestToken()
+        {
+            if (PortalSettings.CustomSettings.ContainsKey("SITESETTINGS_TWITTER_APP_ID") &&
+                !PortalSettings.CustomSettings["SITESETTINGS_TWITTER_APP_ID"].ToString().Equals(string.Empty) &&
+                PortalSettings.CustomSettings.ContainsKey("SITESETTINGS_TWITTER_APP_SECRET") &&
+                !PortalSettings.CustomSettings["SITESETTINGS_TWITTER_APP_SECRET"].ToString().Equals(string.Empty)) {
+                string appId = PortalSettings.CustomSettings["SITESETTINGS_TWITTER_APP_ID"].ToString();
+                var appSecret = PortalSettings.CustomSettings["SITESETTINGS_TWITTER_APP_SECRET"].ToString();
+
+                string server = ConvertRelativeUrlToAbsoluteUrl("~/DesktopModules/CoreModules/SignIn/LogInTweeter.aspx");
+                Session["TwitterAppId"] = appId;
+                Session["TwitterAppSecret"] = appSecret;
+
+                OAuthTokenResponse requestToken = OAuthUtility.GetRequestToken(appId, appSecret, server);
+
+                return requestToken;
+
+            }
+
+            return null;
+        }
+
+        public string ConvertRelativeUrlToAbsoluteUrl(string relativeUrl)
+        {
+
+            if (Request.IsSecureConnection)
+
+                return string.Format("https://{0}{1}", Request.Url.Host, Page.ResolveUrl(relativeUrl));
+
+            else
+
+                return string.Format("http://{0}{1}", Request.Url.Host, Page.ResolveUrl(relativeUrl));
+
+        }
+
+        #endregion
+    }
+}
