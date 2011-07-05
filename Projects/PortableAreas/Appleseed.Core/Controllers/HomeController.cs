c452278f7598103e3d54f1880b287ca647b06b5a
lorena calvo <lorena.calvo@agileworks.com.uy>
2011-07-05 14:57:35 -0300

    Fixed editing title in line

--- Projects/PortableAreas/Appleseed.Core/Controllers/HomeController.cs
+++ Projects/PortableAreas/Appleseed.Core/Controllers/HomeController.cs
@@ -35,42 +35,47 @@ namespace Appleseed.Core.Controllers
             return Json("testString");
         }
 
-        public void SaveTitle(string id, string value){
-            var parse = id.Split('_');
-            int moduleId = Int32.Parse(parse[1]);
-            // Si el usuario tiene permiso para modificar
-            if (PortalSecurity.HasEditPermissions(moduleId)) {
-                PortalSettings settings = (PortalSettings)HttpContext.Items["PortalSettings"];
-                var m = settings.ActivePage.Modules.Cast<ModuleSettings>().FirstOrDefault(mod => mod.ModuleID == moduleId);
+        public ActionResult SaveTitle(string id, string value){
+            try {
+                var parse = id.Split('_');
+                int moduleId = Int32.Parse(parse[1]);
+                // Si el usuario tiene permiso para modificar
+                if (PortalSecurity.HasEditPermissions(moduleId)) {
+                    PortalSettings settings = (PortalSettings)HttpContext.Items["PortalSettings"];
+                    var m = settings.ActivePage.Modules.Cast<ModuleSettings>().FirstOrDefault(mod => mod.ModuleID == moduleId);
 
-                if (m == null) {
-                    return;
+                    if (m == null) {
+                        return Json(new { result = false });
+                    }
+
+                    var modules = new Appleseed.Framework.Site.Data.ModulesDB();
+                    modules.UpdateModule(
+                    m.PageID,
+                    m.ModuleID,
+                    m.ModuleOrder,
+                    m.PaneName,
+                    value,
+                    m.CacheTime,
+                    m.AuthorizedEditRoles,
+                    m.AuthorizedViewRoles,
+                    m.AuthorizedAddRoles,
+                    m.AuthorizedDeleteRoles,
+                    m.AuthorizedPropertiesRoles,
+                    m.AuthorizedMoveModuleRoles,
+                    m.AuthorizedDeleteModuleRoles,
+                    m.ShowMobile,
+                    m.AuthorizedPublishingRoles,
+                    m.SupportWorkflow,
+                    m.AuthorizedApproveRoles,
+                    m.ShowEveryWhere,
+                    m.SupportCollapsable);
+
+                    return Json(new { result = true });
+
                 }
-
-                var modules = new Appleseed.Framework.Site.Data.ModulesDB();
-                modules.UpdateModule(
-                m.PageID,
-                m.ModuleID,
-                m.ModuleOrder,
-                m.PaneName,
-                value,
-                m.CacheTime,
-                m.AuthorizedEditRoles,
-                m.AuthorizedViewRoles,
-                m.AuthorizedAddRoles,
-                m.AuthorizedDeleteRoles,
-                m.AuthorizedPropertiesRoles,
-                m.AuthorizedMoveModuleRoles,
-                m.AuthorizedDeleteModuleRoles,
-                m.ShowMobile,
-                m.AuthorizedPublishingRoles,
-                m.SupportWorkflow,
-                m.AuthorizedApproveRoles,
-                m.ShowEveryWhere,
-                m.SupportCollapsable);
-
-
-
+                return Json(new { result = false });
+            } catch (Exception) {
+                return Json(new { result = false });
             }
         }
     }
