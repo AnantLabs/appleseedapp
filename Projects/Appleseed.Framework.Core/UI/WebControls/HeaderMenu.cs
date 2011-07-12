8db47c3b8b68c8ee64003dd6cb40d7ca2deeeff3
GermanC <german.catarino@agileworks.com.uy>
2011-07-12 09:44:54 -0300

    Fixed scrollbar in login modal

--- Projects/Appleseed.Framework.Core/UI/WebControls/HeaderMenu.cs
+++ Projects/Appleseed.Framework.Core/UI/WebControls/HeaderMenu.cs
@@ -567,16 +567,16 @@ namespace Appleseed.Framework.Web.UI.WebControls
                             iframewidth = "250px";
                             dialogwidth = "280";
                         } else {
-                            iframewidth = "400px";
-                            dialogwidth = "440";
+                            iframewidth = "435px";
+                            dialogwidth = "470";
                         }
                 }
                 string empty = HttpUrlBuilder.BuildUrl("~/DesktopModules/CoreModules/SignIn/empty.htm");
                 string div = this.ClientID + "_logon_dialog";
                 var url = HttpUrlBuilder.BuildUrl("~/DesktopModules/CoreModules/SignIn/SignInPage.aspx?iframe=true");
                 writer.Write(string.Concat("<div id=\"", this.ClientID, "_logon_dialog\" style=\"display:none\" >"));
-                writer.Write(string.Concat("<div id=\"AppleseedLogin\" style=\"height: 300px !important\" >"));
-                writer.Write("<iframe id=\"iframeAppleseedLogin\" src=\""+empty+"\" onload=\"check()\" width=\""+iframewidth+"\" height=\"295px\"></iframe>");
+                writer.Write(string.Concat("<div id=\"AppleseedLogin\" style=\"height: 330px !important\" >"));
+                writer.Write("<iframe id=\"iframeAppleseedLogin\" src=\""+empty+"\" onload=\"check()\" width=\""+iframewidth+"\" height=\"310px\"></iframe>");
                 writer.Write("</div>");
                 
                 //_logonControl.RenderControl(writer);
@@ -600,7 +600,7 @@ namespace Appleseed.Framework.Web.UI.WebControls
                                 autoOpen: false,
                                 modal: true,
                                 width: ",dialogwidth,@",
-                                height: 310,
+                                height: 360,
                                 resizable: false
                             });
 
