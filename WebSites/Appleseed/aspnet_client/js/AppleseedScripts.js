fc0cdf2bcee3bde65735e49029cae55fcecd7f15
GermanC <german.catarino@agileworks.com.uy>
2011-07-14 15:18:06 -0300

    Added Edit HTML content

---
+++ WebSites/Appleseed/aspnet_client/js/AppleseedScripts.js
@@ -0,0 +1,67 @@
+function EditTitleInLine(url){
+	
+	$(".editTitle").editable(submitEdit, { 
+				
+				indicator : "Saving...",
+				tooltip   : "Click to edit...",
+				name : "Editable.FieldName",
+				id   : "elementid",
+				cancel : 'Cancel',
+         		submit : 'OK',
+				type : "text",
+				cssclass : 'CommandButton'
+
+	});
+	
+	function submitEdit(value, settings)
+	{ 
+	   var edits = new Object();
+	   var origvalue = this.revert;
+	   var textbox = this;
+	   var result = value;
+	   // var pagina = data;
+	   edits[settings.name] = [value];
+	   var succ = origvalue;
+	   var returned = $.ajax({
+			   url: url, 
+			   type: "POST",
+			   async: false,
+			   data: {
+					id: textbox.id,
+					value: result
+			   },
+			   success : function(data){
+					
+					if(data.result = 'true'){
+						succ = result;
+						
+					}
+					else{
+						succ = origvalue;
+						
+					};
+					
+			   }
+			   });
+	   return(succ);
+	 }
+	
+}
+
+function editHtml(id, pageID, dialogwidth, dialogheigth, dir) {
+
+        var divDialog = "HtmlModuleDialog" + id;
+        var iframe = "HtmlMoudleIframe" + id;
+		var url = dir+"&pageID"+pageID;
+        $('.' + iframe).attr('src', url );
+        $('.' + divDialog).dialog({
+			width: dialogwidth,
+			height: dialogheigth,
+			resizable : false
+		});
+
+
+    }
+	
+               
+            
