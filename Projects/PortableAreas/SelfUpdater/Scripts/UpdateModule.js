7303598338b1bacc40922521661ccb1c48ae076f
GermanC <german.catarino@agileworks.com.uy>
2011-08-05 12:13:02 -0300

    Fixed url to support spaces

---
+++ Projects/PortableAreas/SelfUpdater/Scripts/UpdateModule.js
@@ -0,0 +1,67 @@
+﻿/// <reference path="/Scripts/jquery-1.5-vsdoc.js" />
+
+
+
+
+function updateModule(moduleId, schedule) {
+   
+   var actionurl = '/SelfUpdater/Updates/DelayedUpgrade';
+   if (schedule == false) {
+       actionurl = '/SelfUpdater/Updates/RemoveDelayedUpgrade';
+       $('#schedule' + moduleId).show();
+       $('#unschedule' + moduleId).hide();
+   } else {
+       $('#schedule' + moduleId).hide();
+       $('#unschedule' + moduleId).show();
+   }
+
+    $.ajax({
+        url: actionurl,
+        data: {
+            packageId: moduleId
+        },
+        dataType: 'json',
+        timeout: 1200000,
+        success: function (data) {
+            
+        },
+        error: function (data) {
+            trace(data);
+            $('#upgradingDiv').dialog("close");
+            alert("Communication error");
+        }
+    });
+
+    return false;
+}
+
+function applyUpdates() {
+    
+    $('#upgradingDiv').dialog({
+        modal: true,
+        closeOnEscape: false,
+        closeText: '',
+        resizable: false,
+        title: 'Upgrading site',
+        open: function (event, ui) {
+            $(this).closest('.ui-dialog').find('.ui-dialog-titlebar-close').hide();
+        }
+
+    });
+
+    $.ajax({
+        url: '/SelfUpdater/Updates/ApplyUpdates',
+        data: {},
+        dataType: 'json',
+        timeout: 1200000,
+        success: function (data) {
+            $('<li>' + data.msg + '</li>').appendTo('#upgradingUl');
+            window.location.reload();
+        },
+        error: function (data) {
+            trace(data);
+            $('#upgradingDiv').dialog("close");
+            alert("Communication error");
+        }
+    });
+}
---
+++ WebSites/Appleseed/DesktopModules/CommunityModules/SiteMap/Sitemap.ascx
@@ -0,0 +1,2 @@
+<%@ Control Inherits="Appleseed.Content.Web.Modules.Sitemaps" Language="c#" CodeBehind="Sitemap.ascx.cs" %>
+<asp:PlaceHolder ID="PlaceHolder1" runat="server"></asp:PlaceHolder>
