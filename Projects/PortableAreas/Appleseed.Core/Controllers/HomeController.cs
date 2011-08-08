0782fc9a87db5c5a466376dacbc670580335088c
Germ?n Schnyder <german.schnyder@agileworks.com.uy>
2011-08-08 13:49:13 -0300

    Some refactoring

--- Projects/PortableAreas/Appleseed.Core/Controllers/HomeController.cs
+++ Projects/PortableAreas/Appleseed.Core/Controllers/HomeController.cs
@@ -13,23 +13,6 @@ namespace Appleseed.Core.Controllers
    
     public class HomeController :Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult Test()
-        {
-            return View();
-        }
-
-        public ActionResult ListResources()
-        {
-            string[] resources = Assembly.GetExecutingAssembly().GetManifestResourceNames();
-            ViewData.Model = resources;
-            return View();
-        }
-
         public JsonResult TestValue()
         {
             return Json("testString");
