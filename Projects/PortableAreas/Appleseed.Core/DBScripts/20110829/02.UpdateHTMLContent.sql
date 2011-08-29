28775da90400a9970bb65199edc3b818ae365673
Julio Sarachaga <julio.sarachaga@agileworks.com.uy>
2011-08-29 16:51:12 -0300

    Fixed the sql script

--- Projects/PortableAreas/Appleseed.Core/DBScripts/20110829/02.UpdateHTMLContent.sql
+++ Projects/PortableAreas/Appleseed.Core/DBScripts/20110829/02.UpdateHTMLContent.sql
@@ -4,7 +4,5 @@ SET
     FriendlyName = 'HTML Content',
     DesktopSrc = 'DesktopModules/CoreModules/HTMLContent/HTMLModule.ascx'
 WHERE
-    GeneralModDefID = '0B113F51-FEA3-499A-98E7-7B83C192FDBB'
-    
+    GeneralModDefID = '0B113F51-FEA3-499A-98E7-7B83C192FDBB'  
 GO
-
