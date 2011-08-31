986c78f95cc4a485beabb32d0278a54eb1002e0f
Julio <Julio@Julio-PC.agileworks.net>
2011-08-31 15:45:45 -0300

    Changed icon

---
+++ Projects/PortableAreas/SelfUpdater/Scripts/SelfUpdater.js
@@ -0,0 +1,136 @@
+﻿/// <reference path="/Scripts/jquery-1.5-vsdoc.js" />
+
+
+
+
+function updatePackage(packageId, schedule) {
+
+    var actionurl = '/SelfUpdater/Updates/DelayedUpgrade';
+    if (schedule == false) {
+        actionurl = '/SelfUpdater/Updates/RemoveDelayedUpgrade';
+
+        $('#schedule' + packageId).show();
+        $('#schedule' + packageId).parents('tr').first().removeClass('ui-state-highlight ui-corner-all');
+
+        $('#unschedule' + packageId).hide();
+
+    } else {
+        $('#schedule' + packageId).hide();
+        $('#schedule' + packageId).parents('tr').first().addClass('ui-state-highlight ui-corner-all');
+
+        $('#unschedule' + packageId).show();
+    }
+
+    $.ajax({
+        url: actionurl,
+        data: {
+            packageId: packageId
+        },
+        dataType: 'json',
+        timeout: 1200000,
+        success: function (data) {
+
+        },
+        error: function (data) {
+            trace(data);
+            $('#upgradingDiv').dialog("close");
+            alert("Communication error");
+        }
+    });
+
+    return false;
+}
+
+function applyUpdates() {
+
+    $('#upgradingDiv').dialog({
+        modal: true,
+        closeOnEscape: false,
+        closeText: '',
+        resizable: false,
+        title: 'Upgrading site',
+        open: function (event, ui) {
+            $(this).closest('.ui-dialog').find('.ui-dialog-titlebar-close').hide();
+        }
+
+    });
+
+    $.post('/SelfUpdater/Updates/ApplyUpdates')
+    .success(function () {
+        $('<li>Applying updates...</li>').appendTo('#upgradingUl');
+
+        var xhr;
+        var reloading = false;
+        var fn = function () {
+            if (!reloading) {
+                if (xhr && xhr.readystate != 4) {
+                    xhr.abort();
+                }
+                xhr = $.post('/SelfUpdater/Updates/Status').success(function (data) {
+                    if (data.online) {
+                        $('<li>Reloading site...</li>').appendTo('#upgradingUl');
+                        reloading = true;
+                        window.location.reload();
+                    }
+                });
+            }
+        };
+
+        var interval = setInterval(fn, 10000);
+    })
+    .error(function () {
+        trace(data);
+        $('#upgradingDiv').dialog("close");
+        alert("Communication error");
+    });
+
+
+    return false;
+}
+
+function installPackage(packageId, source) {
+
+    $('#installingDiv').dialog({
+        modal: true,
+        closeOnEscape: false,
+        closeText: '',
+        resizable: false,
+        title: 'Install package',
+        open: function (event, ui) {
+            $(this).closest('.ui-dialog').find('.ui-dialog-titlebar-close').hide();
+        }
+
+    });
+
+    $.post('/SelfUpdater/Installation/InstallPackage', { packageId: packageId, source: source })
+    .success(function () {       
+
+        var xhr;
+        var reloading = false;
+        var fn = function () {
+            if (!reloading) {
+                if (xhr && xhr.readystate != 4) {
+                    xhr.abort();
+                }
+                xhr = $.post('/SelfUpdater/Updates/Status').success(function (data) {
+                    if (data.online) {
+                        $('<li>Reloading site...</li>').appendTo('#installingUl');
+                        reloading = true;
+                        window.location.reload();
+                    }
+                });
+            }
+        };
+
+        var interval = setInterval(fn, 10000);
+    })
+    .error(function () {
+        trace(data);
+        $('#installingDiv').dialog("close");
+        alert("Communication error");
+    });
+
+
+    return false;
+
+}
