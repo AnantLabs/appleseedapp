ac056c4da35e3f8253cf491f1eb29b4cc45c2dae
German Catarino <german.catarino@agileworks.com.uy>
2011-09-21 15:48:11 -0300

    Portable Area new add control

---
+++ Projects/PortableAreas/SelfUpdater/Controllers/UpdatesController.cs
@@ -0,0 +1,195 @@
+﻿
+using NuGet;
+using System;
+using System.Configuration;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using SelfUpdater.Models;
+using Appleseed.Framework;
+using System.IO;
+using System.Xml;
+using System.Text;
+using System.Dynamic;
+
+namespace SelfUpdater.Controllers
+{
+    [Authorize]
+    public class UpdatesController : Controller
+    {
+        public UpdatesController()
+        {
+        }
+
+        public ActionResult Module()
+        {
+            SelfUpdaterEntities context = new SelfUpdaterEntities();
+
+            var scheduledUpdates = context.SelfUpdatingPackages.ToList();
+
+            WebProjectManager[] projectManagers = this.GetProjectManagers();
+            List<InstallationState> installed = new List<InstallationState>();
+            foreach (var projectManager in projectManagers) {
+                var installedPackages = this.GetInstalledPackages(projectManager);
+
+                foreach (var installedPackage in installedPackages) {
+                    IPackage update = projectManager.GetUpdate(installedPackage);
+                    InstallationState package = new InstallationState();
+                    package.Installed = installedPackage;
+                    package.Update = update;
+                    package.Source = projectManager.SourceRepository.Source;
+
+                    if (scheduledUpdates.Any(d => d.PackageId == installedPackage.Id)) {
+                        package.Scheduled = true;
+                    }
+
+
+                    if (installed.Any(d => d.Installed.Id == package.Installed.Id)) {
+                        var addedPackage = installed.Where(d => d.Installed.Id == package.Installed.Id).First();
+                        if (package.Update != null) {
+                            if (addedPackage.Update == null || addedPackage.Update.Version < package.Update.Version) {
+                                installed.Remove(addedPackage);
+                                installed.Add(package);
+                            }
+                        }
+                    } else {
+                        installed.Add(package);
+                    }
+                }
+            }
+
+            return base.View(installed);
+        }
+
+        private List<IPackage> GetInstalledPackages(WebProjectManager projectManager)
+        {
+            var packages = projectManager.GetInstalledPackages(string.Empty).ToList();
+
+            return packages;
+        }
+
+        private WebProjectManager[] GetProjectManagers()
+        {
+            string remoteSources = ConfigurationManager.AppSettings["PackageSource"] ?? @"D:\";
+            List<WebProjectManager> managers = new List<WebProjectManager>();
+            foreach (var remoteSource in remoteSources.Split(new string[] { ";" }, StringSplitOptions.RemoveEmptyEntries)) {
+                managers.Add(new WebProjectManager(remoteSource, base.Request.MapPath("~/")));
+            }
+
+            return managers.ToArray();
+        }
+
+        public ActionResult Upgrade(string packageId)
+        {
+            try {
+                WebProjectManager[] projectManagers = this.GetProjectManagers();
+
+                WebProjectManager projectManager = null;
+                IPackage installedPackage = null;
+                foreach (var pm in projectManagers) {
+                    projectManager = pm;
+                    installedPackage = this.GetInstalledPackage(pm, packageId);
+                    if (installedPackage != null) break;
+                }
+
+                IPackage update = projectManager.GetUpdate(installedPackage);
+
+                projectManager.UpdatePackage(update);
+
+                return Json(new {
+                    msg = "Updated " + packageId + " to " + update.Version.ToString() + "!",
+                    updated = true
+                }, JsonRequestBehavior.AllowGet);
+            } catch (Exception exc) {
+                ErrorHandler.Publish(LogLevel.Error, exc);
+
+                return Json(new {
+                    msg = "Error updating " + packageId,
+                    updated = false
+                }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+
+        public ActionResult DelayedUpgrade(string packageId, string source, string version)
+        {
+            SelfUpdaterEntities context = new SelfUpdaterEntities();
+
+            var entity = context.SelfUpdatingPackages.Where(d => d.PackageId == packageId).FirstOrDefault();
+            if (entity == default(SelfUpdatingPackages)) {
+
+                entity = new SelfUpdatingPackages() {
+                    PackageId = packageId,
+                    Source = source,
+                    PackageVersion = version                    
+                };
+
+                context.SelfUpdatingPackages.AddObject(entity);
+                context.SaveChanges();
+            }
+
+            return Json(new {
+                msg = "Package " + packageId + " scheduled to update!",
+                res = true
+            }, JsonRequestBehavior.AllowGet);
+
+        }
+
+        public ActionResult RemoveDelayedUpgrade(string packageId)
+        {
+            SelfUpdaterEntities context = new SelfUpdaterEntities();
+
+            var entity = context.SelfUpdatingPackages.Where(d => d.PackageId == packageId).FirstOrDefault();
+            if (entity != default(SelfUpdatingPackages)) {
+                context.SelfUpdatingPackages.DeleteObject(entity);
+                context.SaveChanges();
+
+
+                return Json(new {
+                    msg = "Package " + packageId + " unscheduled!",
+                    res = true
+                }, JsonRequestBehavior.AllowGet);
+            } else {
+                return Json(new {
+                    msg = "Package " + packageId + " unscheduled!",
+                    res = false
+                }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public void ApplyUpdates()
+        {
+            /*Forcing site restart*/
+            var doc = new XmlDocument();
+            doc.PreserveWhitespace = true;
+            var configFile = Server.MapPath("~/web.config");
+            doc.Load(configFile);
+
+            var writer = new XmlTextWriter(configFile, Encoding.UTF8) { Formatting = Formatting.Indented };
+            doc.Save(writer);
+            writer.Flush();
+            writer.Close();
+            /*....................*/
+        }
+
+        public ActionResult Status()
+        {
+            return Json(new {
+                online = true
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        private IPackage GetInstalledPackage(WebProjectManager projectManager, string packageId)
+        {
+            IPackage package = projectManager.GetInstalledPackages(string.Empty).Where(d => d.Id == packageId).FirstOrDefault();
+
+            if (package == null) {
+                throw new InvalidOperationException(string.Format("The package for package ID '{0}' is not installed in this website. Copy the package into the App_Data/packages folder.", packageId));
+            }
+            return package;
+        }
+    }
+}
---
+++ Projects/PortableAreas/SelfUpdater/Views/Updates/Module.ascx
@@ -0,0 +1,60 @@
+﻿<%@ Control Language="C#" Inherits="System.Web.Mvc.ViewUserControl<List<SelfUpdater.Models.InstallationState>>" %>
+<%@ Import Namespace="MvcContrib" %>
+<script src="<%: Url.Resource("Scripts.SelfUpdater.js") %>" type="text/javascript"></script>
+<link type="text/css" rel="stylesheet" href="<%: Url.Resource("Content.SelfUpdater.css") %>" />
+<div>
+    <% if (Model.Count == 0) {%>
+    There are no packages installed
+    <%} else { %>
+    <table id="installed_packages">
+        <tr>
+            <th>
+            </th>
+            <th>
+                Current
+            </th>
+            <th>
+                Update
+            </th>
+            <th>
+            </th>
+        </tr>
+        <% foreach (var m in Model) {%>
+        <tr class="installed_package">
+            <td class="package_icon">
+                <img src="<%= m.Installed.IconUrl %>" alt="" />
+            </td>
+            <td class="package_info">
+                <%= m.Installed.Id%>&nbsp;<%= m.Installed.Version%>
+            </td>
+            <td class="package_update_info">
+                <%
+               if (m.Update != null) {%>
+                <%: m.Update.Id%>&nbsp;<%: m.Update.Version%>&nbsp;<a id="schedule<%: m.Update.Id %>"
+                    href="javascript:void(0)" onclick="updatePackage('<%: m.Update.Id %>', true,'<%: m.Source %>','<%: m.Update.Version.ToString() %>' )"> Schedule
+                    update</a> <a id="unschedule<%: m.Update.Id %>" href="javascript:void(0)" onclick="updatePackage('<%: m.Update.Id %>', false, '<%: m.Source %>','<%: m.Update.Version.ToString() %>')"
+                        style="display: none">Unschedule update</a>
+                <%} else { %>
+                (none)
+                <%} %>
+                <%if (m.Scheduled) { %>
+                <script type="text/javascript">
+                    updatePackage('<%: m.Installed.Id %>', true, '<%: m.Source %>', '<%: m.Update.Version.ToString() %>');
+                </script>
+                <%}%>
+                <%}%>
+            </td>
+        </tr>
+        <%} %>
+    </table>
+</div>
+<% if (Model.Count > 0) {%>
+<div class="installed_packages_action">
+    <input type="button" id="apply" onclick="javascript:applyUpdates();" value="Apply updates!"
+        class="ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only" />
+</div>
+<%} %>
+<div id="upgradingDiv" style="display: none">
+    <ul id="upgradingUl">
+    </ul>
+</div>
