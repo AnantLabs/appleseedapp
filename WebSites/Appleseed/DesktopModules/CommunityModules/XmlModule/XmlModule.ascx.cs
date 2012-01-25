13f4c22d288cbfb774738ef91f52bc096693b111
German Catarino <german.catarino@agileworks.com.uy>
2012-01-25 11:43:25 -0200

    Change custom image menu to match portal and xml/xsl to support external url

--- WebSites/Appleseed/DesktopModules/CommunityModules/XmlModule/XmlModule.ascx.cs
+++ WebSites/Appleseed/DesktopModules/CommunityModules/XmlModule/XmlModule.ascx.cs
@@ -17,6 +17,9 @@ namespace Appleseed.Content.Web.Modules
     using Appleseed.Framework;
     using Appleseed.Framework.DataTypes;
     using Appleseed.Framework.Web.UI.WebControls;
+    using System.Net;
+    using System.Xml;
+    using System.Xml.Xsl;
 
     /// <summary>
     /// Xml Module
@@ -70,52 +73,110 @@ namespace Appleseed.Content.Web.Modules
         {
             base.OnLoad(e);
 
-            var pt = new PortalUrlDataType { Value = this.Settings["XMLsrc"].ToString() };
-            var xmlsrc = pt.FullPath;
+            var xmlSetting = this.Settings["XMLsrc"].ToString();
 
-            if (!string.IsNullOrEmpty(xmlsrc))
-            {
-                if (File.Exists(this.Server.MapPath(xmlsrc)))
-                {
-                    this.xml1.DocumentSource = xmlsrc;
+            if (xmlSetting.StartsWith("http://") || xmlSetting.StartsWith("https://")) {
+                try {
+                    var wr = (HttpWebRequest)WebRequest.Create(xmlSetting);
 
-                    // Change - 28/Feb/2003 - Jeremy Esland
-                    // Builds cache dependency files list
-                    this.ModuleConfiguration.CacheDependency.Add(this.Server.MapPath(xmlsrc));
+                    // set the HTTP properties
+                    wr.Timeout = 300 * 1000; // milliseconds to seconds
+
+                    // Read the response
+                    var resp = wr.GetResponse();
+
+                    // Stream read the response
+                    var stream = resp.GetResponseStream();
+                    if (stream != null) {
+                        // Read XML data from the stream
+                        // ignore the DTD (resolver null)
+                        var reader = new XmlTextReader(stream) { XmlResolver = null };
+
+                        // Create a new document object
+                        var doc = new XmlDocument();
+
+                        // Create the content of the XML Document from the XML data stream
+                        doc.Load(reader);
+
+                        // the XML control to hold the generated XML document
+                        this.xml1.Document = doc; 
+                    }
                 }
-                else
-                {
+                catch (Exception ex) {
+                    this.Controls.Add(
+                            new LiteralControl(
+                                string.Format(
+                                    "<br /><span class='Error'>{0}<br />",
+                                    General.GetString("FILE_NOT_FOUND").Replace("%1%", xmlSetting))));
+                    ErrorHandler.Publish(LogLevel.Error, ex);
+                }
+            }
+            else {
+                var pt = new PortalUrlDataType { Value = this.Settings["XMLsrc"].ToString() };
+                var xmlsrc = pt.FullPath;
+
+                if (!string.IsNullOrEmpty(xmlsrc)) {
+                    if (File.Exists(this.Server.MapPath(xmlsrc))) {
+                        this.xml1.DocumentSource = xmlsrc;
+
+                        // Change - 28/Feb/2003 - Jeremy Esland
+                        // Builds cache dependency files list
+                        this.ModuleConfiguration.CacheDependency.Add(this.Server.MapPath(xmlsrc));
+                    }
+                    else {
+                        this.Controls.Add(
+                            new LiteralControl(
+                                string.Format(
+                                    "<br /><span class='Error'>{0}<br />",
+                                    General.GetString("FILE_NOT_FOUND").Replace("%1%", xmlsrc))));
+                    }
+                }
+            }
+
+            var xslSetting = this.Settings["XSLsrc"].ToString();
+            if (xslSetting.StartsWith("http://") || xslSetting.StartsWith("https://")) {
+                try {                    
+
+                    var t = new XslTransform();
+                    t.Load(xslSetting);
+                    xml1.Transform = t;
+
+                }
+                catch (Exception ex) {
+
                     this.Controls.Add(
                         new LiteralControl(
                             string.Format(
-                                "<br /><span class='Error'>{0}<br />", 
-                                General.GetString("FILE_NOT_FOUND").Replace("%1%", xmlsrc))));
+                                "<br /><span class='Error'>{0}<br />",
+                                General.GetString("FILE_NOT_FOUND").Replace("%1%", xslSetting))));
+                    ErrorHandler.Publish(LogLevel.Error, ex);
                 }
+            
+            
+            
             }
+            else {
+                var pt = new PortalUrlDataType { Value = this.Settings["XSLsrc"].ToString() };
+                var xslsrc = pt.FullPath;
 
-            pt = new PortalUrlDataType { Value = this.Settings["XSLsrc"].ToString() };
-            var xslsrc = pt.FullPath;
+                if (string.IsNullOrEmpty(xslsrc)) {
+                    return;
+                }
 
-            if (string.IsNullOrEmpty(xslsrc))
-            {
-                return;
-            }
+                if (File.Exists(this.Server.MapPath(xslsrc))) {
+                    this.xml1.TransformSource = xslsrc;
 
-            if (File.Exists(this.Server.MapPath(xslsrc)))
-            {
-                this.xml1.TransformSource = xslsrc;
-
-                // Change - 28/Feb/2003 - Jeremy Esland
-                // Builds cache dependency files list
-                this.ModuleConfiguration.CacheDependency.Add(this.Server.MapPath(xslsrc));
-            }
-            else
-            {
-                this.Controls.Add(
-                    new LiteralControl(
-                        string.Format(
-                            "<br /><span class='Error'>{0}<br />", 
-                            General.GetString("FILE_NOT_FOUND").Replace("%1%", xslsrc))));
+                    // Change - 28/Feb/2003 - Jeremy Esland
+                    // Builds cache dependency files list
+                    this.ModuleConfiguration.CacheDependency.Add(this.Server.MapPath(xslsrc));
+                }
+                else {
+                    this.Controls.Add(
+                        new LiteralControl(
+                            string.Format(
+                                "<br /><span class='Error'>{0}<br />",
+                                General.GetString("FILE_NOT_FOUND").Replace("%1%", xslsrc))));
+                }
             }
         }
 
