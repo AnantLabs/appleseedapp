6dd8224889939be4879dff9a83f1373a0ad976b4
German Catarino <german.catarino@agileworks.com.uy>
2012-08-28 10:50:41 -0300

    Adding Id to de default MapRoute

--- WebSites/Appleseed/Global.asax.cs
+++ WebSites/Appleseed/Global.asax.cs
@@ -80,7 +80,7 @@ namespace Appleseed
             routes.MapRoute(
                  "Default",                                              // Route name
                  "{controller}/{action}/{id}",                           // URL with parameters
-                 new { controller = "Home", action = "Index", id = "" } // Parameter defaults
+                 new { controller = "Home", action = "Index", id = "1" } // Parameter defaults
              );
         }
 
