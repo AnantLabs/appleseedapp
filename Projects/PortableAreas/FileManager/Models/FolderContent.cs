7ee8b37e3b64df5a9728d378a3730f6777976b1c
German Catarino <german.catarino@agileworks.com.uy>
2012-10-03 11:06:40 -0300

    Creating Branch, listing directories

---
+++ Projects/PortableAreas/FileManager/Models/FolderContent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FileManager.Models {
+    public class FolderContent
+    {
+
+        public List<String> Files;
+        public List<String> Folders;
+
+        public FolderContent()
+        {
+
+            Files = new List<string>();
+            Folders = new List<string>();
+
+        }
+    }
+}
