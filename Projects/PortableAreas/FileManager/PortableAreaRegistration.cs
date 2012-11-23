064be330be6286184977ac77338be4094eb44a33
German Catarino <german.catarino@agileworks.com.uy>
2012-11-23 10:11:53 -0200

    Mvc 4 initial commit

---
+++ Projects/PortableAreas/FileManager/PortableAreaRegistration.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+using System.Web.Mvc;
+using Appleseed.Core;
+using Appleseed.Framework.Core.Model;
+using MvcContrib.PortableAreas;
+
+namespace FileManager {
+    public class PortableAreaMvc3Registration : MvcContrib.PortableAreas.PortableAreaRegistration {
+        public override string AreaName {
+            get {
+                return "FileManager";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context, IApplicationBus bus) {
+            context.MapRoute("PortableAreaMvc3_ResourceRoute", "FileManager/resource/{resourceName}",
+               new { controller = "EmbeddedResource", action = "Index" },
+               new string[] { "MvcContrib.PortableAreas" });
+
+            context.MapRoute("PortableAreaMvc3_ResourceImageRoute", "FileManager/images/{resourceName}",
+              new { controller = "EmbeddedResource", action = "Index", resourcePath = "images" },
+              new string[] { "MvcContrib.PortableAreas" });
+
+            context.MapRoute(
+                "PortableAreaMvc3_default",
+                "FileManager/{controller}/{action}/{id}",
+                new { action = "Index", controller = "Home", id = UrlParameter.Optional }
+            );
+
+            var assemblyName = Assembly.GetAssembly(this.GetType()).FullName;
+
+
+            var generalModuleDefId = ModelServices.RegisterPortableAreaModule(AreaName, assemblyName, "Home");
+            ModelServices.AddModuleToPortal(generalModuleDefId, 0);
+
+            this.RegisterAreaEmbeddedResources();
+            PortableAreaUtils.RegisterScripts(this, context, bus);
+
+
+        }
+
+    }
+}
---
+++ Projects/PortableAreas/SelfUpdater/Controllers/BaseController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NuGet;
+using System.Dynamic;
+using System.Configuration;
+
+namespace SelfUpdater.Controllers
+{
+    public class BaseController : Controller
+    {
+
+        protected WebProjectManager[] GetProjectManagers()
+        {
+            string remoteSources = ConfigurationManager.AppSettings["PackageSource"] ?? @"D:\";
+            List<WebProjectManager> managers = new List<WebProjectManager>();
+            foreach (var remoteSource in remoteSources.Split(new string[] { ";" }, StringSplitOptions.RemoveEmptyEntries)) {
+                managers.Add(new WebProjectManager(remoteSource, base.Request.MapPath("~/")));
+            }
+
+            return managers.ToArray();
+        }
+
+        protected List<IPackage> GetInstalledPackages(WebProjectManager projectManager)
+        {
+            var packages = projectManager.GetInstalledPackages(string.Empty).ToList();
+
+            return packages;
+        }
+
+        protected List<IPackage> GetAvailablePackages(WebProjectManager projectManager)
+        {
+            var packages = projectManager.GetRemotePackages(string.Empty).ToList();
+
+            return packages;
+        }
+
+    }
+}
