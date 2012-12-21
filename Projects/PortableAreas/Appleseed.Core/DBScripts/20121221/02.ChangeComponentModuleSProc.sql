04b268f00c02f1c3483cfc6a8ae1e12eb7391feb
German Catarino <german.catarino@agileworks.com.uy>
2012-12-21 11:44:45 -0200

    Changing SPROC ComponentModule

--- Projects/PortableAreas/Appleseed.Core/DBScripts/20121221/02.ChangeComponentModuleSProc.sql
+++ Projects/PortableAreas/Appleseed.Core/DBScripts/20121221/02.ChangeComponentModuleSProc.sql
@@ -1,17 +1,11 @@
-﻿
-/****** Object:  StoredProcedure [rb_UpdateComponentModule]    Script Date: 12/21/2012 10:41:31 AM ******/
+﻿/****** Object:  StoredProcedure [rb_UpdateComponentModule]    Script Date: 12/21/2012 11:43:14 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-IF EXISTS (SELECT * FROM sysobjects WHERE id = OBJECT_ID(N'[rb_UpdateComponentModule]') AND OBJECTPROPERTY(id, N'IsProcedure') = 1)
-DROP PROCEDURE [rb_UpdateComponentModule]
-GO
-
-
 /* Procedure rb_UpdateComponentModule*/
-CREATE PROCEDURE rb_UpdateComponentModule
+ALTER PROCEDURE [rb_UpdateComponentModule]
 	@ModuleID int,
 	@CreatedByUser nvarchar(100),
 	@Title nvarchar(100),
