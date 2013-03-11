ce9e9a3375271507e9eea61ca8ecaa8829ea45eb
Bettina Neira <bettina.neira@agileworks.com.uy>
2013-03-11 16:15:47 -0300

    Add Events module

--- WebSites/Appleseed/Setup/Scripts/1.2.8.1739.sql
+++ WebSites/Appleseed/Setup/Scripts/1.2.8.1739.sql
@@ -31,27 +31,27 @@
 -- Install script, updated Events
 -----------------------
 
---DECLARE @GeneralModDefID uniqueidentifier
---DECLARE @FriendlyName nvarchar(128)
---DECLARE @DesktopSrc nvarchar(256)
---DECLARE @MobileSrc nvarchar(256)
---DECLARE @AssemblyName varchar(50)
---DECLARE @ClassName nvarchar(128)
---DECLARE @Admin bit
---DECLARE @Searchable bit
+DECLARE @GeneralModDefID uniqueidentifier
+DECLARE @FriendlyName nvarchar(128)
+DECLARE @DesktopSrc nvarchar(256)
+DECLARE @MobileSrc nvarchar(256)
+DECLARE @AssemblyName varchar(50)
+DECLARE @ClassName nvarchar(128)
+DECLARE @Admin bit
+DECLARE @Searchable bit
 
---SET @GeneralModDefID = '{EF9B29C5-E481-49A6-9383-8ED3AB42DDA0}'
---SET @FriendlyName = 'Events'
---SET @DesktopSrc = 'DesktopModules/Events/Events.ascx'
---SET @MobileSrc = 'MobileModules/Events.aspx'
---SET @AssemblyName = 'Appleseed.DLL'
---SET @ClassName = 'Appleseed.Content.Web.ModulesEvents'
---SET @Admin = 0
---SET @Searchable = 1
+SET @GeneralModDefID = '{EF9B29C5-E481-49A6-9383-8ED3AB42DDA0}'
+SET @FriendlyName = 'Events'
+SET @DesktopSrc = 'DesktopModules/Events/Events.ascx'
+SET @MobileSrc = 'MobileModules/Events.aspx'
+SET @AssemblyName = 'Appleseed.DLL'
+SET @ClassName = 'Appleseed.Content.Web.ModulesEvents'
+SET @Admin = 0
+SET @Searchable = 1
 
----- update module
---EXEC [rb_AddGeneralModuleDefinitions] @GeneralModDefID, @FriendlyName, @DesktopSrc, @MobileSrc, @AssemblyName, @ClassName, @Admin, @Searchable
---GO
+-- update module
+EXEC [rb_AddGeneralModuleDefinitions] @GeneralModDefID, @FriendlyName, @DesktopSrc, @MobileSrc, @AssemblyName, @ClassName, @Admin, @Searchable
+GO
 
---INSERT INTO [rb_Versions] ([Release],[Version],[ReleaseDate]) VALUES('1739','1.2.8.1739', CONVERT(datetime, '02/10/2003', 101))
---GO
+INSERT INTO [rb_Versions] ([Release],[Version],[ReleaseDate]) VALUES('1739','1.2.8.1739', CONVERT(datetime, '02/10/2003', 101))
+GO
