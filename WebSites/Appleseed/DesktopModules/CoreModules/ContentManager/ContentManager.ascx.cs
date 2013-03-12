86eac55fd0bd9efed1b2a31621f52a34fd557ee4
Bettina Neira <bettina.neira@agileworks.com.uy>
2013-03-12 14:13:17 -0300

    Fixing exclude Events module from project.

--- WebSites/Appleseed/DesktopModules/CoreModules/ContentManager/ContentManager.ascx.cs
+++ WebSites/Appleseed/DesktopModules/CoreModules/ContentManager/ContentManager.ascx.cs
@@ -446,18 +446,18 @@ namespace Appleseed.Content.Web.Modules
         /// <param name="stateSaver"></param>
         public override void Install(IDictionary stateSaver)
         {
-            //string currentScriptName = Path.Combine(Server.MapPath(TemplateSourceDirectory), "install.sql");
+            string currentScriptName = Path.Combine(Server.MapPath(TemplateSourceDirectory), "install.sql");
 
-            //List<string> errors = DBHelper.ExecuteScript(currentScriptName, true);
-            //if (errors.Count > 0)
-            //{
-            //    // Call rollback
-            //    throw new Exception("Error occurred:" + errors[0].ToString());
-            //}
+            List<string> errors = DBHelper.ExecuteScript(currentScriptName, true);
+            if (errors.Count > 0)
+            {
+                // Call rollback
+                throw new Exception("Error occurred:" + errors[0].ToString());
+            }
 
-            //DirectoryInfo installDir =
-            //    new DirectoryInfo(Path.Combine(Server.MapPath(TemplateSourceDirectory), "InstallScripts"));
-            //FileInfo[] installFiles = installDir.GetFiles("*_Install.sql");
+            DirectoryInfo installDir =
+                new DirectoryInfo(Path.Combine(Server.MapPath(TemplateSourceDirectory), "InstallScripts"));
+            FileInfo[] installFiles = installDir.GetFiles("*_Install.sql");
             //foreach (FileInfo scriptToInstall in installFiles)
             //{
             //    currentScriptName = scriptToInstall.FullName;
