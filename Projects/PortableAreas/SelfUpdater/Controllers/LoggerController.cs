4393c8e7844a1d77f77f9424f96f5b78892f79a7
German Catarino <german.catarino@agileworks.com.uy>
2013-04-05 10:24:46 -0300

    SelfUpdater - Fixing Ui Percentaje

--- Projects/PortableAreas/SelfUpdater/Controllers/LoggerController.cs
+++ Projects/PortableAreas/SelfUpdater/Controllers/LoggerController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 using NuGet;
+using SelfUpdater.Models;
 
 namespace SelfUpdater.Controllers
 {
@@ -30,7 +31,7 @@ namespace SelfUpdater.Controllers
             msg += message;
 
             IHubContext hub = GlobalHost.ConnectionManager.GetHubContext<SignalR.SelfUpdaterHub>();
-            hub.Clients.All.nuevoProcentaje(msg);
+            //hub.Clients.All.nuevoProcentaje(msg);
 
 
             // Lo escribo en un archivo para ver que anda
@@ -44,58 +45,66 @@ namespace SelfUpdater.Controllers
             }
             catch(Exception){}
 
-            if (!list.ContainsKey(msg)) {
-                list.Add(msg, msg);
-            }
+            //if (!list.ContainsKey(msg)) {
+            //    list.Add(msg, msg);
+            //}
 
 
             // Creating message
 
-                if (msg.Contains("Starting") && !list.ContainsKey("install")) {
-                list.Add("install", msg);
-            }
+            //if (msg.Contains("Starting") && !list.ContainsKey("install")) {
+            //    list.Add("install", msg);
+            //}
             if(message.Contains("Downloading")){
                 int index = message.IndexOf("...");
+                string mssg = message.Substring(0, index + 3);
+                var pct = message.Substring(index + 3, message.Length - (index + 3));
+                var model = new PercentajeModel {Msg = mssg, Pct = pct};
+                hub.Clients.All.newPercentaje(model);
 
-                if (!list.ContainsKey("download")) {
-                    string mssg = message.Substring(0, index + 3);
-                    list.Add("download", mssg);
-                    list.Add("percent", message.Substring(index+2,message.Length - (index + 2)));
+                //if (!list.ContainsKey("download")) {
+                //    string mssg = message.Substring(0, index + 3);
+                //    list.Add("download", mssg);
+                //    list.Add("percent", message.Substring(index+2,message.Length - (index + 2)));
 
-                }
-                else {
-                    list.Remove("percent");
-                    list.Add("percent", message.Substring(index + 2, message.Length - (index + 2)));
-                }
+                //}
+                //else {
+                //    list.Remove("percent");
+                //    list.Add("percent", message.Substring(index + 2, message.Length - (index + 2)));
+                //}
             }
-            if (message.Contains("Successfully added")) {
-                if (list.ContainsKey("Successful"))
-                {
-                    list["Successful"] = message;
-                }
-                else
-                {
-                    list.Add("Successful", message);
-                }
+            else
+            {
+                hub.Clients.All.newMessage(msg);
+            }
+            //if (message.Contains("Successfully added")) {
+            //    if (list.ContainsKey("Successful"))
+            //    {
+            //        list["Successful"] = message;
+            //    }
+            //    else
+            //    {
+            //        list.Add("Successful", message);
+            //    }
                 
-            }
-            if (message.Contains("Waiting to Reload Site")) {
-                list.Add("Waiting", message);
-            }
-            var mensaje = String.Empty;
-            if (list.ContainsKey("install"))
-                mensaje = "<li>" + list["install"] + "</li>";
-            if (list.ContainsKey("download") && list.ContainsKey("percent")) {
-                mensaje += "<li>" + list["download"]  + list["percent"] + "%" + "</li>";
-                //if (list["percent"].Contains("100"))
-                //    mensaje += "<li> Installing package ... </li>";
-            }
-            if (list.ContainsKey("Successful")) {
-                mensaje += "<li>" + list["Successful"] + "</li>";
-            }
-            if (list.ContainsKey("Waiting")) {
-                mensaje += "<li>" + list["Waiting"] + "</li>";
-            }
+            //}
+            //if (message.Contains("Waiting to Reload Site")) {
+            //    list.Add("Waiting", message);
+            //}
+            //var mensaje = String.Empty;
+            //if (list.ContainsKey("install"))
+            //    mensaje = "<li>" + list["install"] + "</li>";
+            //if (list.ContainsKey("download") && list.ContainsKey("percent")) {
+            //    mensaje += "<li>" + list["download"]  + list["percent"] + "%" + "</li>";
+            //    //if (list["percent"].Contains("100"))
+            //    //    mensaje += "<li> Installing package ... </li>";
+            //}
+            //if (list.ContainsKey("Successful")) {
+            //    mensaje += "<li>" + list["Successful"] + "</li>";
+            //}
+            //if (list.ContainsKey("Waiting")) {
+            //    mensaje += "<li>" + list["Waiting"] + "</li>";
+            //}
 
             //HttpContext.Current.Application["NugetLogger"] = mensaje;
             
