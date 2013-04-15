8b51aade984375864e84e2e72b239b51fcdf26be
Bettina Neira <bettina.neira@agileworks.com.uy>
2013-04-15 10:06:15 -0300

    Adding plugin to ckeditor.

---
+++ WebSites/Appleseed/aspnet_client/ckeditor/plugins/addnewitem/plugin.js
@@ -0,0 +1,23 @@
+﻿
+
+CKEDITOR.plugins.add('addnewitem',
+{
+    init: function (editor) {
+        var pluginName = 'addnewitem';
+        editor.ui.addButton('Addnewitem',
+            {
+                label: 'Add new item',
+                command: 'Addnewitem',
+                icon: CKEDITOR.plugins.getPath('addnewitem') + 'accordion.png'
+            });
+        var cmd = editor.addCommand('Addnewitem', { exec: showMyDialog });
+    }
+});
+function showMyDialog(e) {
+    e.insertHtml(
+        "<h3>New panel</h3>" +
+         "<div>New data<p>" +
+         "</div>"   
+    );
+}
+
