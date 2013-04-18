b591091fd201324affc33f1afbe7a959a46cfab5
Bettina Neira <bettina.neira@agileworks.com.uy>
2013-04-18 14:33:42 -0300

    CKEditor. Inserting one accordion with one item.

--- WebSites/Appleseed/aspnet_client/ckeditor/plugins/accordion/plugin.js
+++ WebSites/Appleseed/aspnet_client/ckeditor/plugins/accordion/plugin.js
@@ -1,43 +1,25 @@
 ﻿(function(){
-CKEDITOR.plugins.add('accordion',
-{
-    init: function (editor) {
-        var pluginName = 'accordion';
-        editor.ui.addButton('Accordion',
-            {
-                label: 'Accordion',
-                command: 'Accordion',
-                icon: CKEDITOR.plugins.getPath('accordion') + 'accordion.png',
-            });
-        var cmd = editor.addCommand('Accordion', { exec: showMyDialog });
-    }
-});
-function showMyDialog(e) {
-    var textarea = CKEDITOR.instances.Content_ctl01.getData();
-    var exist = textarea.toString().indexOf('class="accordion"');
-    if (exist != -1) {
-        var newitem = textarea.toString().replace('<!--ITEM-->', '<div class="accordion-head">This is where the accordion header goes.</div>' +
-                '<div class="accordion-body">' +
-                    '<p>This is where the accordion body goes.</p>' +
-                '</div><!--ITEM-->');
-        CKEDITOR.instances.Content_ctl01.setData(newitem);
-    } else {
-
+    CKEDITOR.plugins.add('accordion',
+    {
+        init: function (editor) {
+            var pluginName = 'accordion';
+            editor.ui.addButton('Accordion',
+                {
+                    label: 'Accordion',
+                    command: 'Accordion',
+                    icon: CKEDITOR.plugins.getPath('accordion') + 'accordion.png',
+                });
+            var cmd = editor.addCommand('Accordion', { exec: showMyDialog });
+        }
+    });
+    function showMyDialog(e) {
         e.insertHtml(
             '<div class="accordion">' +
                 '<div class="accordion-head">This is where the accordion header goes.</div>' +
                 '<div class="accordion-body">' +
                     '<p>This is where the accordion body goes.</p>' +
-                '</div><!--ITEM--><br><br>' +
-            '</div>' +
-            '<script>' +
-            '$(".accordion").accordion({'+
-                'collapsible: true,' +
-                'active: false, ' +
-                'alwaysOpen: false, ' +
-            '});'+
-            '</script>'
-        );
+                '</div>' +
+            '</div><br><br>'
+        ); 
     }
-}
 })();
