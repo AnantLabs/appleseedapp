768e3aed9854e6850703e85b08368b2cfb49c549
Bettina Neira <bettina.neira@agileworks.com.uy>
2013-05-03 10:10:37 -0300

    User Manager. Adding search and add new user.

--- Projects/PortableAreas/UserManager/Models/UserManagerModel.cs
+++ Projects/PortableAreas/UserManager/Models/UserManagerModel.cs
@@ -8,6 +8,7 @@ namespace UserManager.Models
     public class UserManagerModel
     {
         public Guid UserId { get; set; }
+        public int id { get; set; }
         public string UserName { get; set; }
         public string UserEmail { get; set; }
         public string UserRol { get; set; }
